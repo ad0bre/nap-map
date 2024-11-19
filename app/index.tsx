2c8aa451ed37a749de30e64c3e31fa7e81e519c7
Andrei Dobre <Andrei.Dobre@theaccessgroup.com>
2024-11-19 07:59:54 +0200

    Added firebase authentication

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,53 @@
 import { Text, View } from "react-native";
+import { firebase } from "./../firebaseConfig";
+import { useState, useEffect, SetStateAction } from "react";
+import { FirebaseAuthTypes } from "@react-native-firebase/auth";
+
+type User = FirebaseAuthTypes.User;
+type UserCredential = FirebaseAuthTypes.UserCredential;
 
 export default function Index() {
+  const [user, setUser] = useState<User | null>(null);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user: User) => {
+      setUser(user);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const handleSignUp = () => {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((userCredential: UserCredential) => {
+        setUser(userCredential.user);
+      })
+      .catch((error: Error) => {
+        console.error(error);
+      });
+  };
+
+  const handleSignIn = () => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((userCredential: UserCredential) => {
+        setUser(userCredential.user);
+      })
+      .catch((error: Error) => {
+        console.error(error);
+      });
+  };
+
+  const handleSignOut = () => {
+    firebase.auth().signOut()
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error: Error) => {
+        console.error(error);
+      });
+  };
+  
   return (
     <View
       style={{
