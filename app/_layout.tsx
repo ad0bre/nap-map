ed71c3a0c9116018b4ba387b67e8f2934a204fae
Andrei Dobre <andrei.dobr3@gmail.com>
2025-01-12 21:22:37 +0200

    Addded routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
+import { AppContextProvider } from "./context/AppContext";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+  <AppContextProvider>
+    <Stack>
+      <Stack.Screen name="index" options={{headerShown: false}}/>
+      <Stack.Screen name="LoginPage" options={{headerShown: false}}/>
+      <Stack.Screen name="SignUpPage" options={{headerShown: false}}/>
+      <Stack.Screen name="HomeScreen" options={{headerShown: false, navigationBarHidden: true}}/>
+    </Stack>;
+  </AppContextProvider>
+  );
 }
